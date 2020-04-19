43d27057cdef312aff749039635b3732b45ae212
Andrea Magni <andrea.magni@gmail.com>
2020-04-19 09:20:52 +0200

    * Last changes from the live session of April, 18th

--- fmxclient/Data.Remote.dfm
+++ fmxclient/Data.Remote.dfm
@@ -2,4 +2,28 @@ object RemoteData: TRemoteData
   OldCreateOrder = False
   Height = 268
   Width = 377
+  object RESTClient1: TRESTClient
+    Accept = 'application/json, text/plain; q=0.9, text/html;q=0.8,'
+    AcceptCharset = 'utf-8, *;q=0.8'
+    BaseURL = 'https://apocalypse.chapmanworld.com/api'
+    ContentType = 'application/json'
+    Params = <>
+    Left = 152
+    Top = 48
+  end
+  object GameRequest: TRESTRequest
+    Client = RESTClient1
+    Method = rmPOST
+    Params = <>
+    Resource = 'games'
+    Response = GameResponse
+    SynchronizedEvents = False
+    Left = 144
+    Top = 112
+  end
+  object GameResponse: TRESTResponse
+    ContentType = 'application/json'
+    Left = 144
+    Top = 168
+  end
 end
--- fmxclient/Data.Remote.pas
+++ fmxclient/Data.Remote.pas
@@ -3,14 +3,24 @@ unit Data.Remote;
 interface
 
 uses
-  System.SysUtils, System.Classes;
+  System.SysUtils, System.Classes, REST.Types, REST.Client,
+  Data.Bind.Components, Data.Bind.ObjectScope
+, datamodel
+;
 
 type
+  TErrorProc = reference to procedure (const AMessage: string);
+
   TRemoteData = class(TDataModule)
+    RESTClient1: TRESTClient;
+    GameRequest: TRESTRequest;
+    GameResponse: TRESTResponse;
   private
     { Private declarations }
   public
     { Public declarations }
+    procedure CreateGame(const AGameData: IGameData; const AOnSuccess: TProc<string>;
+      const AOnError: TErrorProc);
   end;
 
 var
@@ -21,5 +31,38 @@ implementation
 {%CLASSGROUP 'FMX.Controls.TControl'}
 
 {$R *.dfm}
+
+uses
+  System.JSON, REST.JSON
+,  datamodel.standard
+;
+
+{ TRemoteData }
+
+procedure TRemoteData.CreateGame(const AGameData: IGameData;
+  const AOnSuccess: TProc<string>; const AOnError: TErrorProc);
+var
+  LJSON: string;
+begin
+  LJSON := TJson.ObjectToJsonString(AGameData.AsObject);
+
+//  GameRequest.Method := rmPOST;
+  GameRequest.Body.Add(LJSON, ctAPPLICATION_JSON); // actual body of the request, application/json
+//  GameRequest.Params.ParameterByName('body').Value := LJSON; // application/x-www-form-urlencoded
+  GameRequest.ExecuteAsync(
+    procedure
+    begin
+      if Assigned(AOnSuccess) then
+        AOnSuccess(GameResponse.Content);
+    end
+   , True, True
+   , procedure(AObj: TObject)
+     begin
+      if Assigned(AOnError) then
+        AOnError('ERROR! ' + AObj.ToString);
+     end
+  );
+
+end;
 
 end.
